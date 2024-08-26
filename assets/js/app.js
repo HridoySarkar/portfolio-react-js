c9391e447ab6846a1b3afe71bd4307302922d044
Hridoy <khanh4572@gmail.com>
2024-08-26 14:40:53 +0600

    section add & design

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+    try {
+        // $(selector).countMe(delay,speed)
+        $(".counter_box1").countMe(100, 100);
+        $(".counter_box2").countMe(100, 200);
+        $(".counter_box3").countMe(300, 500);
+        $(".counter_box4").countMe(100, 200);
+    } catch (error) {
+        console.log(error);
+    }
+
+    try {
+        new Filterizr('.gallery_container',{});
+    } catch (error) {
+        console.log(error);
+    }
+}
---
+++ assets/js/dependancy.js
@@ -0,0 +1,49 @@
+var conter_list = [
+    {
+        counter_amount: 140,
+        counter_unit: "K",
+        counter_label_top: "Happy",
+        counter_label_bottom: "Clients",
+        image: './assets/images/1.jpg',
+    },
+    {
+        counter_amount: 60,
+        counter_unit: "+",
+        counter_label_top: "Successful",
+        counter_label_bottom: "Projects",
+        image: './assets/images/2.webp',
+    },
+    {
+        counter_amount: 5,
+        counter_unit: "+",
+        counter_label_top: "Years",
+        counter_label_bottom: "Experince",
+        image: './assets/images/3.webp',
+    },
+    {
+        counter_amount: 15,
+        counter_unit: "+",
+        counter_label_top: "Partners",
+        counter_label_bottom: "",
+        image: './assets/images/4.webp',
+    }
+];
+
+var gallery_images = [
+    {
+        image: './assets/images/img1.jpg',
+        category: 'all',
+    },
+    {
+        image: './assets/images/img4.png',
+        category: 'ux',
+    },
+    {
+        image: './assets/images/img1.jpg',
+        category: 'design'
+    },
+    {
+        image: './assets/images/img4.png',
+        category: 'development',
+    },
+]
